fc964dc5572a2c98ab05be26d471c911ca44a868
B-urb <bjoclurban@gmail.com>
2019-01-28 21:39:40 +0100

    Fixed Work Description

--- src/components/WorkHistory.js
+++ src/components/WorkHistory.js
@@ -15,41 +15,29 @@ const WorkHistory = () => (
     </Header>
     <List inverted>
     <List.Item>
-    <Image avatar src='https://react.semantic-ui.com/images/avatar/small/rachel.png' />
+    <Image avatar src='%PUBLIC_URL%/zf_logo.svg' />
     <List.Content>
     <List.Header as='a'>ZF Group</List.Header>
     <List.Description>
-        Last seen watching{' '}
-        <a>
-            <b>Arrested Development</b>
-        </a>{' '}
-        just now.
+        Apr 2018 - today
     </List.Description>
     </List.Content>
 </List.Item>
 <List.Item>
-    <Image avatar src='https://react.semantic-ui.com/images/avatar/small/rachel.png' />
+    <Image avatar src='https://auto-logos.com/wp-content/uploads/2018/01/porsche-logo.png' />
     <List.Content>
     <List.Header as='a'>Porsche AG</List.Header>
     <List.Description>
-        Last seen watching{' '}
-        <a>
-            <b>Arrested Development</b>
-        </a>{' '}
-        just now.
+        Sep 2017 - Mar 2019
     </List.Description>
     </List.Content>
 </List.Item>
 <List.Item>
-    <Image avatar src='https://react.semantic-ui.com/images/avatar/small/rachel.png' />
+    <Image avatar src='https://upload.wikimedia.org/wikipedia/de/9/9e/Hochtief-Logo.svg' />
     <List.Content>
     <List.Header as='a'>Hochtief AG</List.Header>
     <List.Description>
-        Last seen watching{' '}
-        <a>
-            <b>Arrested Development</b>
-        </a>{' '}
-        just now.
+       Oct 2015 - Sep 2017
     </List.Description>
     </List.Content>
 </List.Item>
