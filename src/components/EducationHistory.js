504d80a7a4edf96f5af4f26d52cd1d848bbf02b0
B-urb <bjoclurban@gmail.com>
2019-04-14 22:02:13 +0200

    Various images added and UI changes

--- src/components/EducationHistory.js
+++ src/components/EducationHistory.js
@@ -14,15 +14,15 @@ const EducationHistory = () => (
             Education
         </Header>
 
-        <List inverted>
+        <List inverted size={'massive'} divided>
             <List.Item>
-                <Image avatar src='https://react.semantic-ui.com/images/avatar/small/rachel.png' />
+                <Image style={{'font-size':42}} avatar src='https://www.xing.com/img/custom/cp/assets/logo/2/4/1/12865/square_512px/UNI_Bonn_Logo_Standard_RZ_L.jpg' />
                 <List.Content>
                     <List.Header as='a'>Rheinische Friedrichs Wilhelms Universität Bonn</List.Header>
                 </List.Content>
             </List.Item>
             <List.Item>
-                <Image avatar src='https://react.semantic-ui.com/images/avatar/small/rachel.png' />
+                <Image style={{'font-size':42}} avatar src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Ruhr-Universit%C3%A4t_Bochum_logo.svg/2000px-Ruhr-Universit%C3%A4t_Bochum_logo.svg.png' />
                 <List.Content>
                     <List.Header as='a'>Ruhr-Universität Bochum</List.Header>
                 </List.Content>
--- src/components/WorkHistory.js
+++ src/components/WorkHistory.js
@@ -13,36 +13,48 @@ const WorkHistory = () => (
     <Header inverted>
         Work
     </Header>
-    <List inverted>
+    <List inverted size={'massive'} divided>
     <List.Item>
-    <Image avatar src='%PUBLIC_URL%/zf_logo.svg' />
-    <List.Content>
-    <List.Header as='a'>ZF Group</List.Header>
-    <List.Description>
-        Apr 2018 - today
-    </List.Description>
-    </List.Content>
+        {/*<Image avatar src='%PUBLIC_URL%/zf_logo.svg' />*/}
+<Image style={{fontSize:42}} avatar src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/ZF_logo_STD_Blue_3CC.svg/1024px-ZF_logo_STD_Blue_3CC.svg.png'/>
+<List.Content>
+<List.Header as='a'>ZF Group</List.Header>
+<List.Description>
+Apr 2018 - today
+</List.Description>
+</List.Content>
 </List.Item>
 <List.Item>
-    <Image avatar src='https://auto-logos.com/wp-content/uploads/2018/01/porsche-logo.png' />
-    <List.Content>
-    <List.Header as='a'>Porsche AG</List.Header>
-    <List.Description>
-        Sep 2017 - Mar 2019
-    </List.Description>
-    </List.Content>
+<Image style={{fontSize:42}} avatar src='https://lezebre.lu/images/detailed/79/45326-Sticker-Porsche-Logo.png' />
+<List.Content>
+<List.Header as='a'>Porsche AG</List.Header>
+<List.Description>
+Sep 2017 - Mar 2019
+</List.Description>
+</List.Content>
 </List.Item>
 <List.Item>
-    <Image avatar src='https://upload.wikimedia.org/wikipedia/de/9/9e/Hochtief-Logo.svg' />
-    <List.Content>
-    <List.Header as='a'>Hochtief AG</List.Header>
+<Image style={{fontSize:42}} avatar src='https://cdn.worldvectorlogo.com/logos/hochtief.svg' />
+<List.Content>
+<List.Header as='a'>Hochtief AG</List.Header>
+<List.Description>
+Oct 2015 - Sep 2017
+</List.Description>
+
     <List.Description>
-       Oct 2015 - Sep 2017
+        <List bulleted size={'tiny'}>
+            <List.Item>
+                Did this
+            </List.Item>
+            <List.Item>
+                Did That
+            </List.Item>
+        </List>
     </List.Description>
-    </List.Content>
+</List.Content>
 </List.Item>
 </List>
-    </Container>
+</Container>
 );
 
 export default WorkHistory;
