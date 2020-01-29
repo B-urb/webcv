339bd9aa9098ef0ca205a04d57baea0040cbbc09
Björn Urban <bjoclurban@gmail.com>
2020-01-29 17:06:57 +0100

    Removed Deprecated Components

---
+++ src/components/AboutMe.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+import {Container, Segment} from "semantic-ui-react"
+
+
+const AboutMe = () => (
+    <Segment>
+        I am a software Dev
+    </Segment>
+)
