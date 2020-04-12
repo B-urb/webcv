ef5580c1b46bee665c502771b689bf4bc8046589
Björn Urban <bjoclurban@gmail.com>
2020-04-12 23:08:05 +0200

    Removed Unnecessary imports

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Container, Header, Segment} from "semantic-ui-react"
+import {Container, Header} from "semantic-ui-react"
 
 
 class AboutMe extends React.Component {
