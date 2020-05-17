950342952520a03b3f95e854de084313c97bf6d8
Björn Urban <bjoclurban@gmail.com>
2020-05-17 19:41:21 +0200

    Changes to footer

--- src/components/Functional/Footer.js
+++ src/components/Functional/Footer.js
@@ -2,7 +2,9 @@ import React from "react"
 import {Segment} from "semantic-ui-react";
 
 const Footer = () => (
-  <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}> I am a Footer</Segment>
+  <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
+
+  </Segment>
 );
 
 
