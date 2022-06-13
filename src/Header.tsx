5861b34870efbd80ce2faeabb04c6c05f7dcc8e2
Björn Urban <bjoclurban@gmail.com>
2022-06-13 16:41:54 +0200

    Redo of Webcv with Vite, Initial Commit

---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+const Header = () => {
+    return <div className="bg-gray-700 flex flex-col items-center justify-center row-span-1 text-amber-50 text-center text-4xl">
+        <h1>Björn Urban</h1>
+    </div>
+
+
+
+
+}
+
+export default Header;
