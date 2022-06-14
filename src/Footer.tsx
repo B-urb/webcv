a5bff5c862f4a33c97b38f64ddc62af9d4978ccc
Björn Urban <bjoclurban@gmail.com>
2022-06-14 14:49:31 +0200

    Updated styling of page to black and green

---
+++ src/Footer.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+const Footer = () => {
+    return <div className="bg-gradient-to-b border-t-orange-500  from-black to-gray-700 text-4xl text-amber-50 self-stretch w-screen text-center">
+        Ich bin ein Footer
+    </div>
+}
+
+export default Footer;
