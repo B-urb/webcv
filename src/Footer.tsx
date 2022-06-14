33488fcaaddd7f146257645b7af29110b37ab2c3
Björn Urban <bjoclurban@gmail.com>
2022-06-14 19:31:17 +0200

    Added Intro Text and changed Layout

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -6,8 +6,8 @@
 
 
 const Footer = () => {
-    return <div className="bg-gradient-to-b border-t-orange-500  from-black to-gray-700 text-4xl text-amber-50 self-stretch w-screen text-center">
-        Ich bin ein Footer
+    return <div className="flex bg-gradient-to-b justify-center border-t-orange-500 align-text-bottom from-black to-gray-700 text-4xl text-amber-50 self-stretch w-screen text-center">
+        <p className="absolute bottom-0 align-bottom">Ich bin ein Footer</p>
     </div>
 }
 
---
+++ src/IntroText.tsx
@@ -0,0 +1,6 @@
+
+
+const IntroText = () => {
+    return <p className="text-center">Hi, my Name is Björn and I am a software developer</p>
+}
+export default IntroText;
