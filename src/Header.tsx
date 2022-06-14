86f118c4f015f3b3b31b4a99b8622f33b1dded03
Björn Urban <bjoclurban@gmail.com>
2022-06-14 20:04:47 +0200

    Added Icon

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,13 @@
-
-
-
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faCode} from "@fortawesome/free-solid-svg-icons";
+library.add(faCode)
 
 const Header = () => {
+
     return <div className="bg-gradient-to-b from-gray-800 to-black min-h-full self-stretch items-center justify-center text-green-500 text-center text-4xl">
         <h1>Björn Urban</h1>
+        <FontAwesomeIcon icon={faCode}/>
     </div>
 
 
