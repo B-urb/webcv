6e1434bf413e39b1bc5caec15880d4076879b3ee
Björn Urban <bjoclurban@gmail.com>
2022-06-15 18:08:08 +0200

    Updated Styling

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,14 +1,17 @@
-
-
-
-
-
-
-
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faGithub, faLinkedin, faTwitter, faInstagram} from "@fortawesome/free-brands-svg-icons";
+library.add(faGithub, faLinkedin)
 const Footer = () => {
-    return <div className="flex bg-gradient-to-b justify-center border-t-orange-500 align-text-bottom from-black to-gray-700 text-4xl text-amber-50 self-stretch w-screen text-center">
-        <p className="absolute bottom-0 align-bottom">Ich bin ein Footer</p>
-    </div>
+    return <div className="relative bottom-0 from-black to-gray-700 text-4xl text-amber-50 self-stretch w-screen text-center">
+            <ul className="relative bottom-0 inline">
+                <li onClick={()=> window.open("https://github.com")} className="inline m-1"><FontAwesomeIcon icon={faGithub}/></li>
+                <li className="inline m-1"><FontAwesomeIcon icon={faLinkedin}/></li>
+                <li className="inline m-1"><FontAwesomeIcon icon={faTwitter}/></li>
+                <li className="inline m-1"><FontAwesomeIcon icon={faInstagram}/></li>
+            </ul>
+        </div>
+
 }
 
 export default Footer;
