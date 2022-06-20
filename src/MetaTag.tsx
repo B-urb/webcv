7353ec5e97e71963476cbc7977c1a393f1523663
Björn Urban <bjoclurban@gmail.com>
2022-06-20 14:26:29 +0200

    Updated cards
    Updated Cards to use a grid layout. This is done to have a footer row for displaying metadata tags

--- src/MetaTag.tsx
+++ src/MetaTag.tsx
@@ -5,10 +5,10 @@
 
 const MetaTag = (props: {tag: string}) => {
     return (
-        <div className="text-xl rounded-xl bg-slate-500 text-white w-fit p-2 divide-x divide-white">
+        <div className="md:text-xl text-sm rounded-xl bg-slate-500 text-white w-fit p-2 divide-x divide-white">
         <span className="divide-white divide-x-2"><i className="devicon-cplusplus-plain"></i>
 
-            <span className="md:text-md ml-2 mr-1">
+            <span className="ml-2 mr-1 hidden md:visible">
                 {" "+ props.tag}
         </span>
         </span>
--- src/ProjectsCard.tsx
+++ src/ProjectsCard.tsx
@@ -3,9 +3,11 @@ import MetaTag from "./MetaTag";
 
 const ProjectsCard = (props: {key: number, content: string}) => {
     return <div className="dark:shadow-white dark:shadow-md h-full self-stretch rounded-md m-4">
-        <div className=" shadow-sm p-3 rounded-md bg-gray-900 min-w-0 self-stretch h-full max-h-full overflow-scroll text-xs md:text-lg">
-            <p>{props.content}</p>
-            <div><MetaTag tag={"C++"}/></div>
+        <div className=" shadow-sm p-3 rounded-md bg-gray-900 min-w-0 self-stretch h-full max-h-full divide-y divide-white divide-dotted grid overflow-scroll  text-xs md:text-lg">
+            <div className="row-span-1">
+                <h2>Header</h2></div>
+            <p className="row-auto">{props.content}</p>
+            <div className="row-end-auto row-span-1 p-3 flex align-bottom h-fit"><MetaTag tag={"C++"}/></div>
     </div>
     </div>
 }
