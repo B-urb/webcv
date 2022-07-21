9a9275cf3c05753c97772cca8452423005f4b3be
Björn Urban <burban@inovex.de>
2022-07-21 08:36:13 +0200

    Update layout and refactor grid

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,7 +3,7 @@ import {library} from "@fortawesome/fontawesome-svg-core";
 import {faGithub, faLinkedin, faTwitter, faInstagram} from "@fortawesome/free-brands-svg-icons";
 library.add(faGithub, faLinkedin)
 const Footer = () => {
-    return <div className="from-black to-gray-700 text-2xl md:text-4xl dark:text-amber-50">
+    return <div className="dark:bg-gradient-to-b flex justify-center from-black to-gray-700 text-2xl md:text-4xl dark:text-amber-50">
             <ul className="flex">
                 <li key={0} onClick={()=> window.open("https://github.com/B-urb")} className="m-1 sm:m-3 hover:cursor-crosshair"><FontAwesomeIcon icon={faGithub}/></li>
                 <li key={1} onClick={()=> window.open("https://www.linkedin.com/in/bj%C3%B6rn-u-3a9347113")} className="m-1 sm:m-3 hover:cursor-crosshair"><FontAwesomeIcon icon={faLinkedin}/></li>
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -10,26 +10,25 @@ import NavBar from "./NavBar";
 const Layout = () => {
   return (
         <div
-            className="h-screen grid grid-flow-row auto-rows-max overflow-scroll bg-yellow-300 text-black dark:bg-black font-jetbrains dark:text-yellow-300">
-            <div className="grid grid-rows-3 border-b-4 font-roboto border-dashed border-black dark:border-yellow-300">
+            className="min-h-screen grid auto-rows-auto overflow-scroll bg-yellow-300 text-black dark:bg-black font-jetbrains dark:text-yellow-300">
+            <div className="dark:bg-gradient-to-b dark:from-gray-800 dark:to-black grid grid-rows-3 border-b-4 font-roboto border-dashed border-black dark:border-yellow-300">
               <div
-                  className="row-span-2 grid grid-cols-4 justify-between dark:bg-gradient-to-b dark:from-gray-800 dark:to-black items-center text-center md:text-4xl">
+                  className="row-span-1 grid grid-cols-4 justify-between  items-center text-center md:text-4xl">
                 <div/>
                 <Header/>
                 <LightDarkSwitch/>
               </div>
+              <div className="row-span-1"/>
               <div className="row-span-1 text-center items-center">
                 <NavBar/>
               </div>
             </div>
-            <main className="min-h-[65vh] justify-items-stretch justify-self-stretch">
+            <main className="min-h-[70vh]">
               <div className="mt-4">
                 <Outlet/>
               </div>
             </main>
-            <div className="object-bottom flex self-end justify-center">
               <Footer/>
-            </div>
           </div>
 
   )
