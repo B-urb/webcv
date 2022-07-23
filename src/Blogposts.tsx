810c20547f9d7565ee553798016dcc9558566d4a
Björn Urban <burban@inovex.de>
2022-07-23 15:51:01 +0200

    Update and fix Blogposts list

--- src/Blogposts.tsx
+++ src/Blogposts.tsx
@@ -15,11 +15,12 @@ const Blogposts = () => {
       return <span>Error: {error.message}</span>
 
     case "success": return <div className="text-center mt-2 flex items-center justify-center">
-        <ul className="shadow-md rounded-2xl shadow-black dark:shadow-gray-300 group first:hover:rounded-t-2xl last:rounded-b-2xl list-none border-solid border-l-2 border-r-2 border-b-2 border-t-2 w-96 md:w-3/4 justify-center border-black dark:border-amber-50 dark:divide-amber-50 divide-y divide-double">
+        <ul className="shadow-md rounded-2xl shadow-black dark:shadow-gray-300 group overflow-hidden list-none border-solid border-l-2 border-r-2 border-b-2 border-t-2 w-96 md:w-3/4 justify-center border-black dark:border-amber-50 dark:divide-amber-50 divide-y ">
           {data.data != undefined && data.data.length > 0 ?
-              data.data.map((post, key) => <li key={key} className="border-black group-hover:rounded-t-2xl dark:border-gray-50 border-solid  hover:bg-gray-400">
-                <Link to={post.id!.toString()}>{post.title!}</Link>
-              </li>) : <li>No Blogposts yet :( </li>}
+              data.data.map((post, key) => <Link key={key} to={post.id!.toString()}>
+                <li className="border-black dark:border-gray-50 border-solid  hover:bg-gray-400">
+                {post.title!}
+              </li></Link>) : <li>No Blogposts yet :( </li>}
         </ul>
       </div>
     }
