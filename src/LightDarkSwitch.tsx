cd4bb044b1bffc914639ba3797697fad0682c8fc
Björn Urban <burban@inovex.de>
2022-08-03 15:05:56 +0200

    Fix: minor layout fixes

--- src/LightDarkSwitch.tsx
+++ src/LightDarkSwitch.tsx
@@ -21,7 +21,7 @@ const [theme, setTheme] = useState("dark")
       }
     }
   }
-  return <div className="h-10 w-10 mr-4 mt-2 md:h-16 md:w-16 text-md hover:dark:shadow-lg hover:dark:bg-yellow-300 hover:dark:text-black duration-1000 transition-all md:text-2xl md:mr-2 md:mt-2 justify-self-end flex justify-center rounded-full shadow-black dark:shadow-gray-300 shadow-md"><button  onClick={switchTheme} >
+  return <div className="h-8 w-8 mr-4 md:h-14 md:w-14 text-md hover:dark:shadow-lg hover:dark:bg-yellow-300 hover:dark:text-black duration-1000 transition-all text-md md:text-xl md:mr-2 justify-self-end flex justify-center rounded-full shadow-black dark:shadow-gray-300 shadow-md"><button  onClick={switchTheme} >
     <FontAwesomeIcon icon={theme === "dark" ? faLightbulb : faLightbulbOff} />
   </button></div>
 
--- src/ProfileImage.tsx
+++ src/ProfileImage.tsx
@@ -4,9 +4,8 @@ import {getProfileImage} from "./api/directus";
 const ProfileImage = () => {
 const image_uuid = getProfileImage()
 
-  return <img className="rounded-full w-32 h-32 border-4 border-yellow-50"
+  return <img className="rounded-full w-36 h-36 lg:h-56 lg:w-56 border-4 border-yellow-50"
               src={"https://cms.burban.me/assets/"+image_uuid}/>
-
 
 }
 
