2af852aca10c3bb906dff2ba394ebdaf3434d1d1
Björn Urban <burban@inovex.de>
2022-08-11 16:04:03 +0200

    refactor: color scheme adjusted

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,21 @@ module.exports = {
         "jetbrains": ["JetBrains Mono","monospace"],
         "roboto": ["Roboto Condensed","sans-serif"],
         "trispace": ["Trispace", "sans-serif"]
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'light-2': '#136f63',
+        'light-3': '#D72638',
+        'light-4': '#9AD1D4',
+        'light-5': '#102542',
+        'dark-2': '#468189',
+        'dark-3': '#77aca2',
+        'dark-4': '#83781b',
+        'dark-5': '#7b5e7b'
       }
     },
+
   },
   plugins: [require('@tailwindcss/typography')],
 }
