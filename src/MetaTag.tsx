c7d061ac0b09ce9f8617472b6a6fa44a1789a5e6
Björn Urban <burban@inovex.de>
2022-08-11 16:03:23 +0200

    fix: height of the tags

--- src/MetaTag.tsx
+++ src/MetaTag.tsx
@@ -11,8 +11,8 @@ const MetaTag = (props: { tag: string }) => {
   }
 
   return (
-      <div className="rounded-xl bg-slate-500 text-white w-fit flex pl-1 md:pt-0.5 pr-1.5 text-center">
-        <div className="text-center text-base md:text-sm"><i className={findIconOrNull()}></i>
+      <div className="rounded-xl bg-slate-500 text-white h-8 w-fit flex pl-2 pr-2 md:pt-0.5 pr-1.5 text-center">
+        <div className="text-center text-base md:text-lg"><i className={findIconOrNull()}></i>
         </div>
         <span className="ml-0.5 text-sm md:text-base">
                 {"" + props.tag}
