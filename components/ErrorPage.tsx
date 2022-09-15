1c4d4262cdda3a2fd502016f77888710f15eb258
Björn Urban <burban@inovex.de>
2022-09-15 19:06:15 +0200

    fix: readd accidentally deleted files

---
+++ components/ErrorPage.tsx
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+export const ErrorPage = () => {
+
+  return <div className="flex w-full items-center flex-col mt-6"><iframe src="https://giphy.com/embed/6uGhT1O4sxpi8" width="480" height="240" frameBorder="0"
+                 className="giphy-embed" allowFullScreen></iframe>
+    <span className="text-6xl mt-6">404</span>
+  </div>
+}
