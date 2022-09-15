bf48908a9d1003ec3aee9aac23e2f471d85011ea
Björn Urban <burban@inovex.de>
2022-09-15 20:12:23 +0200

    feat: add seo and favicon

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,9 +1,11 @@
 import ProjectsCard from "../../components/ProjectsCard";
 import {allProjects, IProjects} from "../../lib/directus";
+import SEO from "../../components/Seo";
 
 
 const Projects = (props: { projects: Array<IProjects> }) => {
   return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+    <SEO title={"Björn Urban | Projects "} description={"Overview of my personal projects"}/>
     {props.projects != undefined && props.projects.length > 0 ?
         props.projects.map((content, key) => <div key={key} className="flex-col items-stretch flex">
           <ProjectsCard name={content.name!} content={content.description!} tags={content.tags!}/>
