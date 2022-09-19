3c00052d4d07e1c71eda578c6b8a4c0a46fd79e8
Björn Urban <burban@inovex.de>
2022-09-19 16:37:15 +0200

    feat: add sitemap for next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://burban.me',
+  generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false
+  // ...other options
+}
