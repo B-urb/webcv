007b81eee0367f0d53d84c6d5813fbe4bbe382a1
Björn Urban <burban@inovex.de>
2022-10-06 21:06:23 +0200

    fix: remove google fonts in favor of selg hosted because of dsgvo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,10 +9,7 @@ export default function Document() {
       <Html>
         <Head>
           <link rel="icon" type="image/svg+xml" href="/code-solid.svg"/>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"/>
-          <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@100;300&family=JetBrains+Mono&family=Roboto+Condensed:wght@700&family=Trispace:wght@300&display=swap" rel="stylesheet"/>
           <Script type="text/javascript" src="/static/script.js"></Script>
         </Head>
         <body>
