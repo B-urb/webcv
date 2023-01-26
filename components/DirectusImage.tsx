0acd3242ee59e840022f0c60792b79022c5d357e
Björn Urban <burban@inovex.de>
2023-01-26 13:14:27 +0100

    fix: cms url

--- components/DirectusImage.tsx
+++ components/DirectusImage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 
 const directusLoader = ({src, width, quality}: ImageLoaderProps) => {
-  return `https://cms.burban.me/assets/${src}?width=${width}&quality=${quality || 75}`
+  return `https://cms.tecios.de/assets/${src}?width=${width}&quality=${quality || 75}`
 }
 
 const DirectusImage = (props: { src: string, alt: string }) => {
