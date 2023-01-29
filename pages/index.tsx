4ab2cd996c2d64eaf849cade68c71ed9036a385f
Björn Urban <burban@inovex.de>
2023-01-29 22:08:19 +0100

    fix: centering

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ const AboutMe = (props: {data:Introtext}) => {
 
   return <div className="flex flex-col text-xs md:text-xl">
     <NextSeo title={"Björn Urban"} description={"Read about my prior working experience and education, and a little info about me as a person."}/>
-      <div className="w-full flex justify-center">
-        <div className="w-4/5">
+      <div className="w-full flex justify-items-center justify-center ">
+        <div className="w-4/5 flex justify-items-center justify-center ">
     <IntroText introtext={props.data.introtext}/>
         </div>
     </div>
