c9e2b520e10c748713afa763c047272948c5267e
Björn Urban <burban@inovex.de>
2023-06-11 16:49:58 +0200

    feature: pulumi support

---
+++ deploy/src/main/kotlin/myproject/App.kt
@@ -0,0 +1,35 @@
+import com.pulumi.kotlin.Pulumi
+import com.pulumi.kubernetes.apps.v1.kotlin.deploymentResource
+
+fun main() {
+    Pulumi.run { ctx ->
+        val deployment = deploymentResource("nginx") {
+            args {
+                spec {
+                    selector {
+                        matchLabels("app" to "nginx")
+                    }
+                    replicas(1)
+                    template {
+                        metadata {
+                            labels("app" to "nginx")
+                        }
+                        spec {
+                            containers {
+                                name("nginx")
+                                image("nginx")
+                                ports {
+                                    containerPort(80)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        val name = deployment.metadata?.applyValue { it.name.orEmpty() }
+
+        ctx.export("name", name)
+    }
+}
