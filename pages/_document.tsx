b2fb499a788e21f56d28988cd21d5d775d15bedf
Björn Urban <burban@inovex.de>
2023-06-11 17:33:55 +0200

    feature: pulumi support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
           <link rel="icon" type="image/svg+xml" href="/code-solid.svg"/>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"/>
           <Script type="text/javascript" src="/static/script.js" strategy={"beforeInteractive"}></Script>
-          <Script  defer={false} strategy={"beforeInteractive"} data-website-id={process.env.NEXT_PUBLIC_ANALYTICS_ID} src="https://stats.tecios.de/umami.js"/>
+          <Script  defer={false} strategy={"beforeInteractive"} data-website-id={process.env.NEXT_PUBLIC_ANALYTICS_ID} src="https://stats.tecios.de/script.js"/>
         </Head>
         <body>
         <Main />
