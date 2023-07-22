2883fc7243c1fc4bc296fdf2303d2c259fb1a09b
Björn Urban <burban@inovex.de>
2023-07-22 20:55:53 +0200

    fix: pulumi config

--- deploy/index.ts
+++ deploy/index.ts
@@ -29,7 +29,7 @@ const webServerNs = new kubernetes.core.v1.Namespace(resourceName, {metadata: {
     name: resourceName,
 }});
 
-const ingressAnnotation = stackName === "prod" ? {} : basicAuthAnnotation
+const ingressAnnotation = {}//= stackName === "prod" ? {} : basicAuthAnnotation
 
 // Create a new ConfigMap for the Nginx configuration
 
