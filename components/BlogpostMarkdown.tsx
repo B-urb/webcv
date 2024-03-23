c1ff908738c22e8eb0ec9336e8b7310ad566df01
Björn Urban <burban@inovex.de>
2024-03-23 19:00:58 +0100

    fix: linting errors

--- components/BlogpostMarkdown.tsx
+++ components/BlogpostMarkdown.tsx
@@ -1,37 +1,47 @@
-"use client";
+import React from 'react';
+import type { Components } from 'react-markdown';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
-import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+interface CodeProps {
+  className?: string;
+  children?: React.ReactNode;
+}
 
-export const BlogpostMarkdown = (props: { markdown: string }) => {
+const CodeComponent: React.FC<CodeProps> = ({
+  className,
+  children,
+  ...props
+}) => {
+  const match = /language-(\w+)/.exec(className || '');
+  return match ? (
+    <SyntaxHighlighter
+      style={dracula}
+      showLineNumbers
+      language={match[1]}
+      PreTag="div"
+      {...props}
+    >
+      {String(children).replace(/\n$/, '')}
+    </SyntaxHighlighter>
+  ) : (
+    <code className={className} {...props}>
+      {children}
+    </code>
+  );
+};
+
+export const BlogpostMarkdown: React.FC<{ markdown: string }> = ({
+  markdown,
+}) => {
   return (
     <ReactMarkdown
       components={{
-        code({ _, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || "");
-          return match ? (
-            <SyntaxHighlighter
-              // @ts-ignore
-              // TODO: Github issue
-
-              style={dracula}
-              showLineNumbers
-              language={match[1]}
-              PreTag="div"
-              {...props}
-            >
-              {String(children).replace(/\n$/, "")}
-            </SyntaxHighlighter>
-          ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
-          );
-        },
+        code: CodeComponent as Components['code'],
       }}
     >
-      {props.markdown}
+      {markdown}
     </ReactMarkdown>
   );
 };
