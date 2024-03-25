c6da94f37da6449ac84914127119251b1fb7cecf
Björn Urban <burban@inovex.de>
2024-03-25 20:06:08 +0100

    test:  fix e2e test

--- tests/e2e/projects.spec.ts
+++ tests/e2e/projects.spec.ts
@@ -2,21 +2,22 @@ import { expect, test } from "@playwright/test";
 
 test("test", async ({ page }) => {
   await page.goto("http://localhost:3000/projects");
+  await expect(
+    page.getByRole("link", { name: "Doclytics A document analyzer" })
+  ).toBeVisible();
   await expect(page.getByRole("main")).toContainText(
     "A document analyzer using local a local LLM like Ollama for the document management system paperless-ngx"
   );
+  await expect(page.getByRole("main")).toContainText("Doclytics");
   await page
     .getByRole("link", { name: "Doclytics A document analyzer" })
     .click();
   await expect(page.getByRole("paragraph")).toContainText(
     "A document analyzer using local a local LLM like Ollama for the document management system paperless-ngx"
   );
-  await page
-    .getByRole("article")
-    .getByRole("heading", { name: "Doclytics" })
-    .click();
+  await expect(page.getByText("DoclyticsA document analyzer")).toBeVisible();
   await page.getByLabel("Back").click();
-  await expect(page.getByRole("paragraph")).toContainText(
-    "A document analyzer using local a local LLM like Ollama for the document management system paperless-ngx"
-  );
+  await expect(
+    page.getByRole("heading", { name: "Projects summarizing my" })
+  ).toBeVisible();
 });
