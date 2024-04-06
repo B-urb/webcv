3f47160fbd7eb3d81ef9f7797c6d8b5005997641
Björn Urban <burban@inovex.de>
2024-04-06 13:47:34 +0200

    fix: remove console.log

--- components/SkillsGroup.tsx
+++ components/SkillsGroup.tsx
@@ -5,7 +5,6 @@ import SkillsBadge from "./SkillsBadge";
 
 const SkillsGroup = async () => {
   const skills = await allSkills();
-  console.log(skills);
   return skills !== undefined && skills.length > 0 ? (
     <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
       {skills.map((skill) => (
