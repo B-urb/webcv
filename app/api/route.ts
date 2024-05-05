0b78cbd4481f3ca58e027879e7aaf4e1ac534282
Björn Urban <burban@inovex.de>
2024-05-06 00:30:04 +0200

    fix: revalidating data

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,16 +1,15 @@
+import { revalidatePath } from "next/cache";
 import type { NextRequest } from "next/server";
 
 // Helper function to extract and validate the Bearer token
 function extractToken(req: NextRequest): string | null {
   const authHeader = req.headers.get("Authorization");
   if (!authHeader) {
-    console.log("No Authorization header");
     return null;
   }
 
   const parts = authHeader.split(" ");
   if (parts.length !== 2 || parts[0] !== "Bearer") {
-    console.log("Invalid Authorization header format");
     return null;
   }
 
@@ -20,19 +19,27 @@ function extractToken(req: NextRequest): string | null {
 // Unified function to handle both GET and POST with token validation
 async function handleRequest(req: NextRequest) {
   const token = extractToken(req);
-  console.log(token);
-  console.log(process.env.ISR_TOKEN);
   if (!token || token !== process.env.ISR_TOKEN) {
-    console.log("Unauthorized access attempt");
     return new Response("Unauthorized", { status: 401 });
   }
 
   console.log("Authorized");
   try {
     // Assuming some shared logic here for demonstration
+    const data = await req.json();
+    console.log(data);
+    if (data !== undefined) {
+      if (
+        data.operation.contains("create") ||
+        data.operation.contains("delete")
+      ) {
+        revalidatePath(`/${data.collection}}`);
+      } else {
+        revalidatePath(`/${data.collection}/${data.id}`);
+      }
+    }
     return new Response(JSON.stringify({ revalidated: true }), { status: 200 });
   } catch (err) {
-    console.log("Error during revalidation", err);
     return new Response("Error revalidation", { status: 500 });
   }
 }
