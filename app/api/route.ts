48b986b725c7b2e146f194ec871c1199cd4c1bae
Björn Urban <burban@inovex.de>
2024-05-05 14:31:47 +0200

    feat: add ISR Vars

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,45 +1,46 @@
 import type { NextRequest } from "next/server";
 
-export async function GET(req: NextRequest) {
-  // Check for secret to confirm this is a valid request
-  console.log(req);
-  if (req.nextUrl.searchParams.get("secret") !== process.env.ISR_TOKEN) {
-    return new Response("Unauthorized", {
-      status: 401,
-    });
+// Helper function to extract and validate the Bearer token
+function extractToken(req: NextRequest): string | null {
+  const authHeader = req.headers.get("Authorization");
+  if (!authHeader) {
+    console.log("No Authorization header");
+    return null;
   }
 
+  const parts = authHeader.split(" ");
+  if (parts.length !== 2 || parts[0] !== "Bearer") {
+    console.log("Invalid Authorization header format");
+    return null;
+  }
+
+  return parts[1]; // Return the extracted token
+}
+
+// Unified function to handle both GET and POST with token validation
+async function handleRequest(req: NextRequest) {
+  const token = extractToken(req);
+  console.log(token);
+  console.log(process.env.ISR_TOKEN);
+  if (!token || token !== process.env.ISR_TOKEN) {
+    console.log("Unauthorized access attempt");
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  console.log("Authorized");
   try {
-    // this should be the actual path not a rewritten path
-    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    return new Response(JSON.stringify({ revalidated: true }), {
-      status: 200,
-    });
+    // Assuming some shared logic here for demonstration
+    return new Response(JSON.stringify({ revalidated: true }), { status: 200 });
   } catch (err) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
+    console.log("Error during revalidation", err);
     return new Response("Error revalidation", { status: 500 });
   }
 }
-export async function POST(req: NextRequest) {
-  // Check for secret to confirm this is a valid request
-  console.log(req);
-  if (true) {
-    // process.env.MY_SECRET_TOKEN) {
-    return new Response("Unauthorized", {
-      status: 401,
-    });
-  }
 
-  try {
-    // this should be the actual path not a rewritten path
-    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    return new Response(JSON.stringify({ revalidated: true }), {
-      status: 200,
-    });
-  } catch (err) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
-    return new Response("Error revalidation", { status: 500 });
-  }
+export async function GET(req: NextRequest) {
+  return handleRequest(req);
+}
+
+export async function POST(req: NextRequest) {
+  return handleRequest(req);
 }
